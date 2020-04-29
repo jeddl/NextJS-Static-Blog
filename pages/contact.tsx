2bbf785957b6d307de535f76bccce1cfe44a4808
JeDDL <lmetaballsj@gmail.com>
2020-04-28 23:22:49 -0400

    Code clean

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import TopLevelContainer from "../components/TopLevelContainer";
 
 const ContactPage: React.FunctionComponent = () => (
 	<div>
 		<Navbar title="Contact" />
-		<TopLevelContainer title="Contact">
-			<h1>Contact me</h1>
-		</TopLevelContainer>
+		<h1>Contact me</h1>
 	</div>
 );
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import Navbar from "../components/Navbar";
-import TopLevelContainer from "../components/TopLevelContainer";
 
 const IndexPage = () => {
 	return (
 		<>
 			<Navbar title="Home | MetaJeDD"></Navbar>
-			<TopLevelContainer title="Home">
-				<h1>👋 Hello 👋</h1>
-			</TopLevelContainer>
+			<h1>👋 Hello 👋</h1>
 		</>
 	);
 };
