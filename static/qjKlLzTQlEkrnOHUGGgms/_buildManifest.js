0b1c9ae64c0c1e1e4bb2d0b22938ecc146de21dc
JeDDL <lmetaballsj@gmail.com>
2020-04-29 00:06:57 -0400

    Updates

---
+++ static/qjKlLzTQlEkrnOHUGGgms/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/articles/[slug]":[a],"/contact":[a],"/index":[a],"/posts":[a]}}("static\u002Fchunks\u002F08d54e24b20f198b5446852019f58c49465b8245.03815a93e5b8e1473761.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
