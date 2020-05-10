2ef61877645a3b88e4b9403dd7e2250946ea8100
JeDDL <lmetaballsj@gmail.com>
2020-05-10 14:08:04 -0400

    Remove CSSBaseline in TopLevelContainer

--- components/TopLevelContainer.tsx
+++ components/TopLevelContainer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Props from "../interfaces/props";
 import Head from "next/head";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 
 const TopLevelContainer: React.FunctionComponent<Props> = ({
@@ -15,7 +14,6 @@ const TopLevelContainer: React.FunctionComponent<Props> = ({
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
-			<CssBaseline />
 			<Container maxWidth="md">{children}</Container>
 		</>
 	);
