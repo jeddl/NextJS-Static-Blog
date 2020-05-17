4173fecadbe29cfa9dc5540a97e545910b8b7efd
JeDDL <lmetaballsj@gmail.com>
2020-05-16 21:08:25 -0400

    Code clean

--- src/components/styles/NavLink.ts
+++ src/components/styles/NavLink.ts
@@ -2,12 +2,12 @@ const styles = {
 	root: {
 		textDecoration: "none",
 		background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
-		border: 0,
-		borderRadius: 3,
+		border: 2,
+		borderRadius: 9,
 		boxShadow: "0 3px 5px 2px rgba(33, 203, 243, .3)",
 		color: "white",
 		height: 48,
-		padding: "0 30px",
+		padding: "0rem 2rem",
 	},
 };
 
